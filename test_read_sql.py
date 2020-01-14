aa6e611a446b4e7f8719871ee76275e4ab9ee45e
Y <50727312+lianghuiying@users.noreply.github.com>
2020-01-14 16:47:36 +0800

    Delete test_read_sql.py

--- test_read_sql.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-engine = create_engine('mysql+mysqlconnector://root:aA100100!!@47.106.246.213:3306/test?auth_plugin=mysql_native_password', echo=False)
-
-data = pd.read_sql("select * from stocksTricks where symbol in ('sz000005','sz000001','sz000002');",engine)
-print(data)
